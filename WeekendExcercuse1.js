cece335f2554f836f2603e3d27117c4f0befc331
muhammadbsoul <moh.bsoul.93@gmail.com>
2025-12-24 21:10:11 +0200

    solved ex 5.8 + 6.1

--- WeekendExcercuse1.js
+++ WeekendExcercuse1.js
@@ -2,7 +2,7 @@
 
 // EX1
 
-function yesOrNow(boolValue){
+function yesOrNow(boolValue) {
     return boolValue ? "YES" : "NO"
 }
 
@@ -12,32 +12,32 @@ console.log(yesOrNow(true))
 
 // EX2.1
 
-function sumOfLowestNumbers(arrayOfNumbers){
-    let sortedArray = arrayOfNumbers.sort((a,b) => a-b)
+function sumOfLowestNumbers(arrayOfNumbers) {
+    let sortedArray = arrayOfNumbers.sort((a, b) => a - b)
     let sum = 0;
-    for(let i=0; i<2 && i<sortedArray.length; i++) sum += sortedArray[i]
+    for (let i = 0; i < 2 && i < sortedArray.length; i++) sum += sortedArray[i]
     return sum
 }
 
-console.log(sumOfLowestNumbers([3,2,0,8,9,1,200,80000]));
+console.log(sumOfLowestNumbers([3, 2, 0, 8, 9, 1, 200, 80000]));
 
 //////////////////////////////////////////
 
 // EX2.2
 
-function convertBibaryToNumber(binarySeq){
-    return parseInt(binarySeq.join(""),2)
+function convertBibaryToNumber(binarySeq) {
+    return parseInt(binarySeq.join(""), 2)
 }
 
-console.log(convertBibaryToNumber([0,1,0,1]))
+console.log(convertBibaryToNumber([0, 1, 0, 1]))
 
 //////////////////////////////////////////
 
 // EX2.3
 
-function findNextSquare(number){
+function findNextSquare(number) {
     const baseNumber = Math.floor(Math.sqrt(number))
-    return Math.pow(baseNumber,2) === number ? Math.pow(baseNumber+1, 2) : -1
+    return Math.pow(baseNumber, 2) === number ? Math.pow(baseNumber + 1, 2) : -1
 }
 
 console.log(findNextSquare(122))
@@ -46,17 +46,17 @@ console.log(findNextSquare(122))
 
 // EX2.4
 
-function findUniq(numbersArray){
+function findUniq(numbersArray) {
     return numbersArray.find(number => numbersArray.indexOf(number) === numbersArray.lastIndexOf(number))
 }
 
-console.log(findUniq([1,2,2,2]));
+console.log(findUniq([1, 2, 2, 2]));
 
 //////////////////////////////////////////
 
 // EX2.5
 
-function summation(number){
+function summation(number) {
     return number * (1 + number) / 2
 }
 
@@ -66,8 +66,8 @@ console.log(summation(8))
 
 // EX2.6
 
-function centuryFromYear(year){
-    return 1 + Math.floor(year/100)
+function centuryFromYear(year) {
+    return 1 + Math.floor(year / 100)
 }
 
 console.log(centuryFromYear(1993));
@@ -76,7 +76,7 @@ console.log(centuryFromYear(1993));
 
 // EX2.7
 
-function basicOp(operation, num1, num2){
+function basicOp(operation, num1, num2) {
     if (isNaN(num1) && isNaN(num2))
         return "input error"
     if (operation === `+`)
@@ -90,17 +90,17 @@ function basicOp(operation, num1, num2){
     else return "input error"
 }
 
-console.log(basicOp(`+`, 2,6));
+console.log(basicOp(`+`, 2, 6));
 
 //////////////////////////////////////////
 
 // EX3.1
 
-function nb_year(populationStart, increasePersent, fixNewInhabitants, targetPopulation){
+function nb_year(populationStart, increasePersent, fixNewInhabitants, targetPopulation) {
     let currentPopulation = populationStart
     let years = 0
-    while(currentPopulation < targetPopulation){
-        currentPopulation += currentPopulation*increasePersent/100 + fixNewInhabitants
+    while (currentPopulation < targetPopulation) {
+        currentPopulation += currentPopulation * increasePersent / 100 + fixNewInhabitants
         years++;
     }
     return years
@@ -112,8 +112,8 @@ console.log(nb_year(1000, 2, 50, 1200));
 
 // EX5.1
 
-function stringTrim(str){
-    return str.slice(1, str.length-1)
+function stringTrim(str) {
+    return str.slice(1, str.length - 1)
 }
 
 console.log(stringTrim(`NEWTECH`));
@@ -122,9 +122,9 @@ console.log(stringTrim(`NEWTECH`));
 
 // EX5.2
 
-function stringRepeat(repeatCount, str){
+function stringRepeat(repeatCount, str) {
     newStr = ``
-    for (let i=0; i<repeatCount; i++)
+    for (let i = 0; i < repeatCount; i++)
         newStr = newStr.concat(str)
     return newStr;
 }
@@ -135,7 +135,7 @@ console.log(stringRepeat(5, `hello`));
 
 // EX5.5
 
-function abbreviateTwoWords(str){
+function abbreviateTwoWords(str) {
     return str.split(` `).map(word => word[0].toUpperCase()).join(`.`)
 }
 
@@ -145,12 +145,12 @@ console.log(abbreviateTwoWords("muhammad bsoul"));
 
 // EX5.6
 
-function mask(str){
+function mask(str) {
     let newStr = ``;
-    for(let i=0; i<str.length-4; i++) {
+    for (let i = 0; i < str.length - 4; i++) {
         newStr = newStr.concat(`#`);
     }
-    return newStr.concat(str.slice(str.length-4,str.length))
+    return newStr.concat(str.slice(str.length - 4, str.length))
 }
 
 console.log(mask(`nananannananananananaan!!!!`));
@@ -159,7 +159,7 @@ console.log(mask(`nananannananananananaan!!!!`));
 
 // EX5.7
 
-function shortestWordLength(str){
+function shortestWordLength(str) {
     const words = str.split(` `);
     let shortLength = words[0].length
     words.forEach(word => shortLength = word.length < shortLength ? word.length : shortLength)
@@ -169,3 +169,37 @@ function shortestWordLength(str){
 console.log(shortestWordLength(`My name is muhammad`));
 
 //////////////////////////////////////////
+
+// EX5.8
+
+function longestWord(str) {
+    const words = str.split(` `);
+    let longLength = words[0].length
+    words.forEach(word => longLength = word.length > longLength ? word.length : longLength)
+    return words.filter(word => word.length === longLength)
+}
+
+console.log(longestWord(`My name is muhammad`));
+
+//////////////////////////////////////////
+
+// EX6.1
+
+function mumbling(str) {
+    let newStr = ``
+    const letters = str.split(``);
+    let count = 1
+    letters.forEach(char => {
+        newStr = newStr.concat(char.toUpperCase())
+        for (let i = 1; i < count; i++) newStr = newStr.concat(char)
+        count++
+        newStr = newStr.concat(`-`)
+    })
+    return newStr.slice(0,newStr.length-1)
+}
+
+console.log(mumbling(`abcd`));
+
+//////////////////////////////////////////
+
+
